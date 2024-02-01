ed762febaec407ff6826bc657e0f3377227416e4
Ajay v <vajay4834@gmail.com>
2024-02-01 17:26:15 +0530

    new products added

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib import admin
+from django.core.validators import MaxValueValidator
 # Create your models here.
 
 
@@ -19,7 +20,7 @@ class Products(models.Model):
     material = models.CharField(max_length=50)
     warranty = models.CharField(max_length=50)
     brand = models.CharField(max_length=50)
-    rating = models.IntegerField(max=5)
+    rating = models.IntegerField(validators=[MaxValueValidator(5)])
     color = models.CharField(max_length=50)
 
     def discountedPrice(self):
