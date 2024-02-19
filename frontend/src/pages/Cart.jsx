931de24d51c6bbb86a011c0abedd0451f1043fde
Ajay v <vajay4834@gmail.com>
2024-02-19 10:33:05 +0530

    update quantity completed

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -11,7 +11,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import axios from "axios";
+import axios, { Axios } from "axios";
+import Loader from "../components/Loader";
 const Cart = () => {
   const navigate = useNavigate();
   const token = localStorage.getItem("token");
@@ -20,7 +21,6 @@ const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
   const [cartleng, setCartleng] = useState(null);
   const [loading, setLoading] = useState(false);
-  const quantity = 1;
   useEffect(() => {
     if (!token || !id) {
       navigate("/login");
@@ -59,7 +59,7 @@ const Cart = () => {
 
   const averageOfferPercent = Math.floor(totalOffers / cartItems[0]?.length);
   const totalPrice = cartItems[0]?.reduce(
-    (tprice, item) => tprice + item.price,
+    (tprice, item) => ((tprice + item.price)*item.qty),
     0
   );
   const todaysDeal = 2000;
@@ -90,65 +90,51 @@ const Cart = () => {
       toast.error("Erroro Occured ! Try Again");
     }
   };
-  const handleQuantity = async (val) => {
-    if (quantity <= 1) {
-      toast.warning("minimum quantity reached");
-    } else if (quantity >= 10) {
-      toast.warning("maximum quantity reached");
-    } else {
-      if (val === "desc") {
-        quantity -= 1;
+  const handleQuantity = async (itemId, action) => {
+    try {
+      const itemIndex = cartItems[0].findIndex((item) => item.id === itemId);
+      if (itemIndex === -1) {
+        console.log("Item not found in cart");
+        return;
       }
-      if (val === "inc") {
-        quantity += 1;
+      const updatedCartItems = [...cartItems[0]];
+      const itemToUpdate = updatedCartItems[itemIndex];
+      if (action === "desc" && itemToUpdate.qty > 1) {
+        itemToUpdate.qty -= 1;
+      } else if (
+        action === "inc" &&
+        itemToUpdate.qty < itemToUpdate.product.stock_count
+      ) {
+        itemToUpdate.qty += 1;
+        1;
+      } else {
+        toast.warning("Maximum/minimum quantity reached");
+        return;
       }
+      updatedCartItems[itemIndex] = itemToUpdate;
+      setCartItems([updatedCartItems]);
+      const response = await axios.patch(
+        `/api/cart/${itemId}/`,
+        {
+          qty: itemToUpdate.qty,
+        },
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
+          withCredentials: true,
+        }
+      );
+    } catch (error) {
+      console.error("Error updating quantity:", error);
     }
   };
+
   return (
     <>
       {loading ? (
         <>
-          <div className=" p-10 flex justify-center flex-col items-center ">
-            <div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-              <div className="animate-pulse flex space-x-4">
-                <div className="rounded-full bg-slate-700 h-10 w-10"></div>
-                <div className="flex-1 space-y-6 py-1">
-                  <div className="h-2 bg-slate-700 rounded"></div>
-                  <div className="space-y-3">
-                    <div className="grid grid-cols-3 gap-4">
-                      <div className="h-2 bg-slate-700 rounded col-span-2"></div>
-                      <div className="h-2 bg-slate-700 rounded col-span-1"></div>
-                    </div>
-                    <div className="h-2 bg-slate-700 rounded"></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <button
-              disabled
-              type="button"
-              className="py-2.5  mt-5 px-5 me-2 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center"
-            >
-              <svg
-                aria-hidden="true"
-                role="status"
-                className="inline w-4 h-4 me-3 text-gray-200 animate-spin dark:text-gray-600"
-                viewBox="0 0 100 101"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                  fill="currentColor"
-                />
-                <path
-                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                  fill="#1C64F2"
-                />
-              </svg>
-              Loading...
-            </button>
-          </div>
+          <Loader />
         </>
       ) : (
         <>
@@ -227,16 +213,16 @@ const Cart = () => {
                                   <div
                                     className="cursor-pointer border-r px-2"
                                     onClick={() => {
-                                      handleQuantity(dec);
+                                      handleQuantity(item.id, "desc");
                                     }}
                                   >
                                     <FontAwesomeIcon icon={faSubtract} />
                                   </div>
-                                  <div className="px-4">{quantity}</div>
+                                  <div className="px-4">{item.qty}</div>
                                   <div
                                     className="cursor-pointer border-l px-2"
                                     onClick={() => {
-                                      handleQuantity(inc);
+                                      handleQuantity(item.id, "inc");
                                     }}
                                   >
                                     <FontAwesomeIcon icon={faPlus} />
